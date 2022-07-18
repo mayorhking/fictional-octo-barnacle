4d0af4f9729132185b049f208827520ca3863b12
Mayorhking <maydokviv@gmail.com>
2022-07-18 11:46:20 +0100

    correction/modification

---
+++ js-basis/index.js
@@ -0,0 +1,21 @@
+ //this is my javascript practise
+ console.log ('Hello World');
+ let name = 'Mayorhking';
+ console.log(name);
+// VARRIABLE
+ //cannot be a reserved keyword
+ // should be meaningful
+ // variable can not start by numbers
+ //cannot contain a space or hyphen (-)
+ //Are case sensitive
+ //all variable with 2names, first name start with a small latter while the second name starts with capital letter eg. let firstName;
+//  two ways of declearing variables in js eg let firstName = 'Mayorhking', lastName = 'Oguns';
+// or writing them on different lines eg let firstName = 'Mayorhking';
+                                    //  let lastName = 'Oguns';
+
+
+                                    // CONSTANTS
+                                    let interestRate = 0.3;
+                                    interestRate = 1;
+                                    console.log(interestRate);
+
